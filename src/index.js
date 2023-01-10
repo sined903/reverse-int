1718e43f6d4d0a8ba92b9be56c65ae87c9d93213
Denis Kulikov <9933denis@gmail.com>
2023-01-10 22:55:58 +0300

    feat: add the solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let result = [];
+
+  let splitNumb = n.toString().split('');
+
+  splitNumb.forEach(element => {
+    if(element != '-')
+    result.unshift(element)
+  });
+
+  return +result.join('')
+
 }
+
